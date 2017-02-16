f31436d220c5471456d2393a762c7ed81fff41e6
Daniel Puterman <daniel@dapt1.local>
2017-02-16 19:13:07 +0200

    Version up. (v3.7)

--- eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/Eyes.java
+++ eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/Eyes.java
@@ -81,7 +81,7 @@ public class Eyes extends EyesBase {
 
     @Override
     public String getBaseAgentId() {
-        return "eyes.selenium.java/3.6";
+        return "eyes.selenium.java/3.7";
     }
 
     @SuppressWarnings("UnusedDeclaration")
