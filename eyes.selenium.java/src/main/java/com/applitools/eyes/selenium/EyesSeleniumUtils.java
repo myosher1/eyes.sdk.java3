e4e64a1c9032a1d0ac25f97556092e5eab660693
Daniel Puterman <daniel.puterman@applitools.com>
2017-04-26 20:48:07 +0300

    bugfix: better handling of floating point in "getCurrentScrollPosition".

--- eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/EyesSeleniumUtils.java
+++ eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/EyesSeleniumUtils.java
@@ -218,10 +218,9 @@ public class EyesSeleniumUtils {
     public static Location getCurrentScrollPosition(
             JavascriptExecutor executor) {
         //noinspection unchecked
-        List<Double> positionAsList = (List<Double>) executor.executeScript(
-                JS_GET_CURRENT_SCROLL_POSITION);
-        return new Location((int)Math.ceil(positionAsList.get(0)),
-                (int)Math.ceil(positionAsList.get(1)));
+        List<Number> positionAsList = (List<Number>)executor.executeScript(JS_GET_CURRENT_SCROLL_POSITION);
+        return new Location((int)Math.ceil(positionAsList.get(0).doubleValue()),
+                (int)Math.ceil(positionAsList.get(1).doubleValue()));
     }
 
     /**
