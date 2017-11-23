3c021caebeac800162b0ea88a01a89e76c766f56
Daniel Puterman <daniel.puterman@applitools.com>
2017-11-23 16:01:58 +0200

    Updated images fluent test.

--- eyes.images.java/src/test/java/com/applitools/eyes/images/TestImagesApi.java
+++ eyes.images.java/src/test/java/com/applitools/eyes/images/TestImagesApi.java
@@ -20,7 +20,7 @@ public class TestImagesApi {
 
         // Initialize the eyes SDK and set your private API key.
         eyes = new Eyes();
-        eyes.setServerUrl(URI.create("https://localhost.applitools.com"));
+//        eyes.setServerUrl(URI.create("https://localhost.applitools.com"));
         eyes.setApiKey(System.getenv("APPLITOOLS_API_KEY"));
 
         //logHandler = new FileLogger("c:\\temp\\logs\\TestElement.log", true, true);
