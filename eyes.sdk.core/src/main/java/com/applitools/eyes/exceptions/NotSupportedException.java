8970c7cc9a8017351a4a242533e8ade4d7bb23bc
Daniel Puterman <daniel.puterman@applitools.com>
2017-11-26 16:39:56 +0200

    Changed ws.rs.NotSupportedException (not available in Jersey1) to Eyes' specific exception.

---
+++ eyes.sdk.core/src/main/java/com/applitools/eyes/exceptions/NotSupportedException.java
@@ -0,0 +1,18 @@
+package com.applitools.eyes.exceptions;
+
+import com.applitools.eyes.EyesException;
+
+/**
+ * Encapsulates an error when a type or action is not supported.
+ */
+@SuppressWarnings("unused")
+public class NotSupportedException extends EyesException {
+
+    public NotSupportedException(String message) {
+        super(message);
+    }
+
+    public NotSupportedException(String message, Throwable e) {
+        super(message, e);
+    }
+}
