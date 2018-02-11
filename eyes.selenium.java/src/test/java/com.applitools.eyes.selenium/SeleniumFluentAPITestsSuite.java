37834c31c82945758a192f3f78b7ecca3d772cce
Itai Bar-Haim <itai.barhaim@applitools.com>
2018-02-11 17:23:16 +0200

    added category for testing (CI) and using it in maven.

--- eyes.selenium.java/src/test/java/com.applitools.eyes.selenium/SeleniumFluentAPITestsSuite.java
+++ eyes.selenium.java/src/test/java/com.applitools.eyes.selenium/SeleniumFluentAPITestsSuite.java
@@ -1,9 +1,11 @@
 package com.applitools.eyes.selenium;
 
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
+@Category(CI.class)
 @Suite.SuiteClasses({
         TestFluentApi_Chrome.class,
         TestFluentApi_Chrome_ForceFullPageScreenShot.class,
