86a06064327c178657955737d0e5dc4e09449c6f
Itai Bar-Haim <itai.barhaim@applitools.com>
2018-03-29 16:51:25 +0300

    added TestUtils that generates all permutation from a several lists of parameters.

---
+++ eyes.selenium.java/src/test/java/com.applitools.eyes.selenium/TestUtils.java
@@ -0,0 +1,36 @@
+package com.applitools.eyes.selenium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestUtils {
+    public static Object[][] generatePermutations(List<List<Object>> lists) {
+
+        List<Object[]> result = new ArrayList<>();
+
+        generatePermutations(lists, result, 0, null);
+
+        return result.toArray(new Object[0][0]);
+    }
+
+    private static void generatePermutations(List<List<Object>> lists, List<Object[]> result, int depth, List<Object> permutation) {
+        if (depth == lists.size()) {
+            if (permutation != null) {
+                result.add(permutation.toArray());
+            }
+            return;
+        }
+
+        List<Object> listInCurrentDepth = lists.get(depth);
+        for (Object newItem : listInCurrentDepth) {
+            if (permutation == null || depth == 0) {
+                permutation = new ArrayList<>();
+            }
+
+            permutation.add(newItem);
+            generatePermutations(lists, result, depth + 1, permutation);
+            permutation.remove(permutation.size() - 1);
+        }
+    }
+
+}
