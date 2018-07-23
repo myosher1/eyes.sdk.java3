4c900451eed5575738c2064a401e836120ef0e34
Itai Bar-Haim <itai.barhaim@applitools.com>
2018-07-23 14:09:12 +0300

    added proxy support to RemoteSessionEventHandler

--- eyes.connectivity.java.jersey2x/src/main/java/com/applitools/eyes/RemoteSessionEventHandler.java
+++ eyes.connectivity.java.jersey2x/src/main/java/com/applitools/eyes/RemoteSessionEventHandler.java
@@ -17,12 +17,14 @@ public class RemoteSessionEventHandler extends RestClient implements ISessionEve
     private String autSessionId;
 
     private static final String SERVER_SUFFIX = "/applitools/sessions";
-    private final WebTarget defaultEndPoint;
+    private final String accessKey;
+    private WebTarget defaultEndPoint;
     private boolean throwExceptions = true;
 
     @SuppressWarnings("WeakerAccess")
     public RemoteSessionEventHandler(Logger logger, URI serverUrl, String accessKey, int timeout) {
         super(logger, serverUrl, timeout);
+        this.accessKey = accessKey;
         this.defaultEndPoint = endPoint.queryParam("accessKey", accessKey).path(SERVER_SUFFIX);
     }
 
@@ -36,6 +38,11 @@ public class RemoteSessionEventHandler extends RestClient implements ISessionEve
 
     public RemoteSessionEventHandler(URI serverUrl, String accessKey) {
         this(new Logger(), serverUrl, accessKey);
+    }
+
+    public void setProxy(ProxySettings proxySettings){
+        setProxyBase(proxySettings);
+        this.defaultEndPoint = endPoint.queryParam("accessKey", accessKey).path(SERVER_SUFFIX);
     }
 
     private void sendMessage(HttpMethodCall method) {
