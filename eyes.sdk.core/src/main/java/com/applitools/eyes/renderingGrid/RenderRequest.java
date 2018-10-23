60fed6a5297a5e36af6e6deed0dfeac91008af58
Michael Yosher <michael.yosher@applitools.com>
2018-10-23 13:17:31 +0300

    Started working on Rendering grid data objects

---
+++ eyes.sdk.core/src/main/java/com/applitools/eyes/renderingGrid/RenderRequest.java
@@ -0,0 +1,9 @@
+package com.applitools.eyes.renderingGrid;
+
+public class RenderRequest {
+    private String webhook = null;
+    private String url = null;
+    private RGridDom dom = null;
+
+
+}
---
+++ eyes.sdk.core/src/main/java/com/applitools/eyes/renderingGrid/ScreenOrientation.java
@@ -0,0 +1,8 @@
+package com.applitools.eyes.renderingGrid;
+
+public class ScreenOrientation {
+
+    public final String PORTAIT = "portrait";
+
+    public final String LANDSCAPE = "landscape";
+}
