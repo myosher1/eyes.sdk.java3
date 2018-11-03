ba053b0acad7136778d6adbb736c8f6f8ba52247
Daniel Puterman <daniel.puterman@applitools.com>
2018-11-03 12:59:27 +0200

    Version up.

--- eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/Eyes.java
+++ eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/Eyes.java
@@ -142,7 +142,7 @@ public class Eyes extends EyesBase {
 
     @Override
     public String getBaseAgentId() {
-        return "eyes.selenium.java/3.38";
+        return "eyes.selenium.java/3.38.1";
     }
 
     public WebDriver getDriver() {
