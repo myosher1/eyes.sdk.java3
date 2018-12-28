d57dd529d7d77d125dda729a1f87db9da823b453
Itai Bar-Haim <itai.barhaim@applitools.com>
2018-12-28 12:21:57 +0200

    fixed ImageUtils to create missing folder.

--- eyes.selenium.java/src/test/java/com.applitools.eyes.selenium/TestSetup.java
+++ eyes.selenium.java/src/test/java/com.applitools.eyes.selenium/TestSetup.java
@@ -152,7 +152,7 @@ public abstract class TestSetup implements ITest {
         LogHandler logHandler;
 
         if (System.getenv("CI") == null && logsPath != null) {
-            String path = logsPath + File.separator + "java" + File.separator + extendedTestName;
+            String path = logsPath + File.separator + "java" + File.separator + extendedTestName.replaceAll("\\s","_");
             logHandler = new FileLogger(path + File.separator + testName + "_" + platform + ".log", true, true);
             seleniumEyes.setDebugScreenshotsPath(path);
             seleniumEyes.setDebugScreenshotsPrefix(testName + "_");
