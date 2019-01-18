9521d91603b57b100732cea77464a08e32069e63
Daniel Puterman <daniel.puterman@applitools.com>
2019-01-18 22:34:25 +0200

    Version up

--- eyes.selenium.java/src/main/java/com/applitools/eyes/rendering/EyesConnector.java
+++ eyes.selenium.java/src/main/java/com/applitools/eyes/rendering/EyesConnector.java
@@ -96,7 +96,7 @@ class EyesConnector extends EyesBase implements IEyesConnector {
 
     @Override
     protected String getBaseAgentId() {
-        return "eyes.selenium.visualgrid.java/3.143.1";
+        return "eyes.selenium.visualgrid.java/3.144.0";
     }
 
     @Override
