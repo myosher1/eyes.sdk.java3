4d699047149110c10deb6bf10000411f6fc404c8
Itai Bar-Haim <itai.barhaim@applitools.com>
2019-04-21 13:08:03 +0300

    updated VG RCA test

--- eyes.selenium.java/src/test/java/com/applitools/eyes/renderingGrid/TestVisualGridRCA.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/renderingGrid/TestVisualGridRCA.java
@@ -1,5 +1,6 @@
 package com.applitools.eyes.renderingGrid;
 
+import com.applitools.eyes.BatchInfo;
 import com.applitools.eyes.ProxySettings;
 import com.applitools.eyes.selenium.Eyes;
 import com.applitools.eyes.selenium.fluent.Target;
@@ -10,17 +11,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestVisualGridRCA {
+    private BatchInfo batch = new BatchInfo("Test Visual Grid RCA");
+
     @Test
-    public void test_VG_RCA() {
+    public void test_VG_RCA_Config() {
         EyesRunner runner = new VisualGridRunner(10);
         Eyes eyes = new Eyes(runner);
-        eyes.setProxy(new ProxySettings("http://127.0.0.1",8888));
+        eyes.setBatch(batch);
+        //eyes.setProxy(new ProxySettings("http://127.0.0.1",8888));
         WebDriver driver = new ChromeDriver();
         try {
             driver.get("https://applitools.github.io/demo/TestPages/VisualGridTestPage");
-            eyes.open(driver, "Test Visual Grid", "Test RCA");
+            eyes.open(driver, "Test Visual Grid", "Test RCA Config");
             eyes.setSendDom(true);
             eyes.check(Target.window());
+            eyes.close();
+            runner.getAllTestResults();
+        }
+        finally {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test_VG_RCA_Fluent() {
+        EyesRunner runner = new VisualGridRunner(10);
+        Eyes eyes = new Eyes(runner);
+        eyes.setBatch(batch);
+        //eyes.setProxy(new ProxySettings("http://127.0.0.1",8888));
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("https://applitools.github.io/demo/TestPages/VisualGridTestPage");
+            eyes.open(driver, "Test Visual Grid", "Test RCA Fluent");
+            eyes.setSendDom(false);
+            eyes.check(Target.window().sendDom(true));
             eyes.close();
             runner.getAllTestResults();
         }
