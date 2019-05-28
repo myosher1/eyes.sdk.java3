b59a450f2f6ba93d185e096c8ca0fad309922600
Michael Yosher <michael.yosher@applitools.com>
2019-05-28 16:59:12 +0300

    eyes runner in com.applitools.eyes (commons)

---
+++ eyes.common.java/src/main/java/com/applitools/eyes/EyesRunner.java
@@ -0,0 +1,26 @@
+package com.applitools.eyes;
+
+import com.applitools.eyes.LogHandler;
+import com.applitools.eyes.Logger;
+import com.applitools.eyes.visualgrid.model.TestResultSummary;
+
+public abstract class EyesRunner {
+
+    protected Logger logger = new Logger();
+    private String serverUrl;
+
+    public abstract TestResultSummary getAllTestResults();
+
+    public abstract TestResultSummary getAllTestResults(boolean shouldThrowException);
+
+    public void setLogHandler(LogHandler logHandler) {
+        logger.setLogHandler(logHandler);
+        if (!logHandler.isOpen()) {
+            logHandler.open();
+        }
+    }
+
+    public Logger getLogger(){
+      return this.logger;
+    }
+}
