1ae471819652a7548f6b797162191b83e927240d
Michael Yosher <michael.yosher@applitools.com>
2019-05-30 14:56:18 +0300

    new E2E test for Runners with OCC->OCC

--- eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestClassicRunner.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestClassicRunner.java
@@ -1,11 +1,9 @@
 package com.applitools.eyes.selenium;
 
-import com.applitools.eyes.BatchInfo;
-import com.applitools.eyes.ProxySettings;
-import com.applitools.eyes.RectangleSize;
-import com.applitools.eyes.StdoutLogHandler;
+import com.applitools.eyes.*;
 import com.applitools.eyes.selenium.fluent.Target;
 import com.applitools.eyes.visualgrid.model.TestResultSummary;
+import com.applitools.eyes.visualgrid.services.VisualGridRunner;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -13,25 +11,34 @@ import org.testng.annotations.Test;
 public final class TestClassicRunner {
     @Test
     public void test() {
+        ClassicRunner classicRunner = new ClassicRunner();
+        VisualGridRunner VGRunner = new VisualGridRunner(10);
 
+        eyesTest(classicRunner);
+        eyesTest(VGRunner);
+
+    }
+
+    private void eyesTest(EyesRunner runner) {
         // Open a Chrome browser.
         WebDriver driver = new ChromeDriver();
 
         // Initialize the VisualGridEyes SDK and set your private API key.
-        ClassicRunner runner = new ClassicRunner();
         Eyes eyes = new Eyes(runner);
         Eyes eyes2 = new Eyes(runner);
-
-        eyes.setLogHandler(new StdoutLogHandler(true));
+        eyes.setLogHandler(new FileLogger("runnerTest.log", false, true));
+        eyes2.setLogHandler(new FileLogger("runnerTest.log", false, true));
+//        eyes.setLogHandler(new StdoutLogHandler(true));
         eyes.setServerUrl("https://eyes.applitools.com/");
-//        eyes.setProxy(new ProxySettings("http://127.0.0.1:8888"));
+        eyes.setProxy(new ProxySettings("http://127.0.0.1:8888"));
+        eyes2.setProxy(new ProxySettings("http://127.0.0.1:8888"));
 
         // Switch sendDom flag on
         eyes.setSendDom(true);
         eyes2.setSendDom(false);
         eyes2.setStitchMode(StitchMode.CSS);
         eyes.setStitchMode(StitchMode.CSS);
-        BatchInfo batchInfo = new BatchInfo("TTS - config batch");
+        BatchInfo batchInfo = new BatchInfo("Runner Testing");
         batchInfo.setId("RCA_Batch_ID");
         eyes.setBatch(batchInfo);
         eyes2.setBatch(batchInfo);
@@ -51,14 +58,14 @@ public final class TestClassicRunner {
         eyes2.check(Target.window().fully().ignoreDisplacements(false).withName("Step 1"));
 
 
-        eyes.close(true);
+        eyes.closeAsync();
 
         eyes.open(driver, "Applitools Eyes Java SDK", "Classic Runner Test",
                 new RectangleSize(1200, 800));
 
-        eyes.check(Target.window().fully().ignoreDisplacements(false).withName("Step 1"));
+        eyes.check(Target.window().fully().ignoreDisplacements(false).withName("Step 2"));
 
-        eyes.close(true);
+        eyes.closeAsync();
         eyes2.close(true);
 
         driver.quit();
