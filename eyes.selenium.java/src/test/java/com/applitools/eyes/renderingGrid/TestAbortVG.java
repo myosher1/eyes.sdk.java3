b049628846195b445876f60317bb8708840cbf07
Michael Yosher <michael.yosher@applitools.com>
2019-06-30 15:11:54 +0300

    New Test for Abort in VG

---
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/renderingGrid/TestAbortVG.java
@@ -0,0 +1,41 @@
+package com.applitools.eyes.renderingGrid;
+
+import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.visualgrid.model.TestResultContainer;
+import com.applitools.eyes.visualgrid.model.TestResultSummary;
+import com.applitools.eyes.visualgrid.services.VisualGridRunner;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class TestAbortVG {
+    VisualGridRunner runner = new VisualGridRunner(3);
+    private Eyes eyes;
+
+    @Test
+    public void test() {
+        eyes = new Eyes(runner);
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        eyes.open(driver, "Java SDK", "Test Abort");
+        eyes.checkWindow();
+        driver.close();
+    }
+
+    @AfterMethod
+    public void after(ITestResult testContext) {
+        eyes.abortIfNotClosed();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        TestResultSummary allTestResults = runner.getAllTestResults();
+        for (TestResultContainer allTestResult : allTestResults) {
+            if (!allTestResult.getTestResults().isAborted()) {
+                throw new Error("Test not aborted");
+            }
+        }
+    }
+}
