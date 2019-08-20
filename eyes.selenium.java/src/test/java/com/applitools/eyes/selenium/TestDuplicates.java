003bff508a07db472b624e732abcdf9de544cbea
Itai Bar-Haim <itai.barhaim@applitools.com>
2019-08-20 17:00:57 +0300

    added tests

---
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestDuplicates.java
@@ -0,0 +1,29 @@
+package com.applitools.eyes.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class)
+public class TestDuplicates extends TestSetup {
+    @Factory(dataProvider = "dp", dataProviderClass = TestsDataProvider.class)
+    public TestDuplicates(Capabilities caps, String mode) {
+        super("Eyes Selenium SDK - Duplicates", caps, mode);
+        testedPageUrl = "https://applitools.github.io/demo/TestPages/VisualGridTestPage/duplicates.html";
+    }
+
+    @Test
+    public void TestDuplicatedIFrames() {
+        WebDriver driver = getDriver();
+        driver.switchTo().frame(2);
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#p2")));
+        driver.switchTo().defaultContent();
+        getEyes().checkWindow("Duplicated Iframes");
+    }
+}
