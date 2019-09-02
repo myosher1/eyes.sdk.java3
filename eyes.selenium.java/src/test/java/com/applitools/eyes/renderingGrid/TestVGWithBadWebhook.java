10c0fa96ee94ef3385da43e58fd2e0f30e6176df
Itai Bar-Haim <itai.barhaim@applitools.com>
2019-09-02 10:07:25 +0300

    added TestVGWithBadWebhook

---
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/renderingGrid/TestVGWithBadWebhook.java
@@ -0,0 +1,47 @@
+package com.applitools.eyes.renderingGrid;
+
+import com.applitools.eyes.BatchInfo;
+import com.applitools.eyes.RectangleSize;
+import com.applitools.eyes.selenium.Configuration;
+import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.selenium.fluent.Target;
+import com.applitools.eyes.utils.SeleniumUtils;
+import com.applitools.eyes.visualgrid.services.VisualGridRunner;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestVGWithBadWebhook {
+    @Test
+    public void Test() {
+        WebDriver driver = SeleniumUtils.createChromeDriver();
+        driver.get("https://applitools.com/helloworld");
+
+        BatchInfo batch = new BatchInfo("Visual Grid - Test bad webhook");
+        final VisualGridRunner runner = new VisualGridRunner(10);
+        final Eyes eyes = new Eyes(runner);
+
+        Configuration config = new Configuration();
+        config.setBatch(batch);
+        config.setAppName("Visual Grid Tests");
+        config.setTestName("Bad Webhook");
+        config.setViewportSize(new RectangleSize(800, 600));
+
+        eyes.setConfiguration(config);
+        eyes.open(driver);
+        eyes.check(Target.window().fully().beforeRenderScreenshotHook("gibberish uncompilable java script"));
+        driver.quit();
+        Error ex = Assert.expectThrows(Error.class, new Assert.ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                eyes.close();
+                runner.getAllTestResults();
+            }
+        });
+        Assert.assertNotNull(ex);
+        Assert.assertNotNull(ex.getMessage());
+        Assert.assertTrue(ex.getMessage().startsWith(
+                "Render Failed for RenderBrowserInfo{ViewportSize={Width=800, Height=600}, BrowserType=CHROME, EmulationInfo=, Target='FullPage'} "),
+        "Actual string was: " + ex.getMessage());
+    }
+}
