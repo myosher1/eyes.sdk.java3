caf6656d28a60de87f1dc86fe0cd0fd3e231475c
Itai Bar-Haim <itai.barhaim@applitools.com>
2019-09-03 17:44:06 +0300

    fixed TestServerStatus tests.

--- eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestServerStatus.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestServerStatus.java
@@ -1,73 +1,72 @@
 package com.applitools.eyes.selenium;
 
+import com.applitools.eyes.BatchInfo;
 import com.applitools.eyes.RectangleSize;
-import com.applitools.eyes.exceptions.DiffsFoundException;
-import com.applitools.eyes.exceptions.NewTestException;
+import com.applitools.eyes.TestResults;
 import com.applitools.eyes.selenium.fluent.Target;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-import org.openqa.selenium.Platform;
+import com.applitools.eyes.utils.SeleniumUtils;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.UUID;
-
 
 public class TestServerStatus {
 
-    private Eyes eyes;
-    private WebDriver webDriver;
+    @Test
+    public void TestSessionSummary_Status_Failed() {
+        WebDriver webDriver = SeleniumUtils.createChromeDriver();
+        webDriver.get("https://applitools.github.io/demo/TestPages/DynamicResolution/mobile.html");
 
-    @BeforeClass
-    public void setup() throws MalformedURLException {
-        eyes = new Eyes();
-        eyes.setSaveNewTests(false);
+        String uuid = "_" + UUID.randomUUID().toString();
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("disable-infobars");
-
-        DesiredCapabilities caps = new DesiredCapabilities("chrome","", Platform.ANY);
-        caps.setCapability(ChromeOptions.CAPABILITY, options);
-        caps.setCapability("username", System.getenv("SAUCE_USERNAME"));
-        caps.setCapability("accesskey", System.getenv("SAUCE_ACCESS_KEY"));
-
-        webDriver = new RemoteWebDriver(new URL(System.getenv("SELENIUM_SERVER_URL")), caps);
-    }
-
-    private void teardown() {
+        Eyes eyes = new Eyes();
+        eyes.setSaveNewTests(true);
+        eyes.open(webDriver,
+                TestServerStatus.class.getSimpleName(),
+                TestServerStatus.class.getSimpleName(), new RectangleSize(800, 600));
+        eyes.setBatch(TestsDataProvider.batchInfo);
         try {
-            eyes.close();
+            eyes.check("TestSessionSummary_Status_Failed" + uuid, Target.window().fully(false));
+            eyes.close(false);
+        } finally {
+            eyes.abortIfNotClosed();
+        }
+
+        webDriver.get("https://applitools.github.io/demo/TestPages/DynamicResolution/desktop.html");
+
+        eyes.open(webDriver,
+                TestServerStatus.class.getSimpleName(),
+                TestServerStatus.class.getSimpleName(), new RectangleSize(800, 600));
+        try {
+            eyes.check("TestSessionSummary_Status_Failed" + uuid, Target.window().fully(false));
+            TestResults results = eyes.close(false);
+            Assert.assertTrue(results.isDifferent());
         } finally {
             eyes.abortIfNotClosed();
             webDriver.quit();
         }
     }
 
-    @Test(expectedExceptions = DiffsFoundException.class)
-    public void TestSessionSummary_Status_Failed() throws MalformedURLException {
-        WebDriver driver = eyes.open(webDriver,
-                TestServerStatus.class.getSimpleName(),
-                TestServerStatus.class.getSimpleName(), new RectangleSize(800, 599));
-
-        driver.get("http://applitools.github.io/demo/TestPages/FramesTestPage/");
-        eyes.check("TestSessionSummary_Status_Failed", Target.window());
-        teardown();
-    }
-
-    @Test(expectedExceptions = NewTestException.class)
-    public void TestSessionSummary_Status_New() throws MalformedURLException {
+    @Test
+    public void TestSessionSummary_Status_New() {
+        Eyes eyes = new Eyes();
+        eyes.setSaveNewTests(false);
+        WebDriver webDriver = SeleniumUtils.createChromeDriver();
         String uuid = "_" + UUID.randomUUID().toString();
 
         WebDriver driver = eyes.open(webDriver,
                 TestServerStatus.class.getSimpleName() + uuid,
-                TestServerStatus.class.getSimpleName() + uuid, new RectangleSize(800, 599));
+                TestServerStatus.class.getSimpleName() + uuid, new RectangleSize(800, 600));
 
         driver.get("http://applitools.github.io/demo/TestPages/FramesTestPage/");
-        eyes.check("TestSessionSummary_Status_New" + uuid, Target.window());
-        teardown();
+        try {
+            eyes.check("TestSessionSummary_Status_New" + uuid, Target.window());
+            TestResults results = eyes.close(false);
+            Assert.assertTrue(results.isNew());
+        } finally {
+            eyes.abortIfNotClosed();
+            webDriver.quit();
+        }
     }
 }
