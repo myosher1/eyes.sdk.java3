c0802f64f472db5f9f5a8b228c254ef77a8774f3
Itai Bar-Haim <itai.barhaim@applitools.com>
2019-09-08 17:00:03 +0300

    fixed setOverflow injected script.

--- eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/EyesSeleniumUtils.java
+++ eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/EyesSeleniumUtils.java
@@ -243,7 +243,6 @@ public class EyesSeleniumUtils {
 
         String script = String.format("var origOF = arguments[0].style.overflow;" +
                 "arguments[0].style.overflow = '%s';" +
-                "return origOF;" +
                 "if ('%s'.toUpperCase() === 'HIDDEN' && origOF.toUpperCase() !== 'HIDDEN') arguments[0].setAttribute('data-applitools-original-overflow',origOF);" +
                 "return origOF;", value, value);
 
