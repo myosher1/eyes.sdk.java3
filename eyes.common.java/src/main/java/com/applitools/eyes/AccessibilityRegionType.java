281445c53a6e12c0e550e9c239b4f14984699538
Michael Yosher <michael.yosher@applitools.com>
2019-09-22 10:21:36 +0300

    Fix for Enum to send the right values when serializing

--- eyes.common.java/src/main/java/com/applitools/eyes/AccessibilityRegionType.java
+++ eyes.common.java/src/main/java/com/applitools/eyes/AccessibilityRegionType.java
@@ -1,14 +1,28 @@
 package com.applitools.eyes;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum AccessibilityRegionType {
-    None ,
-    RegularText ,
-    LargeText ,
-    BoldText ,
-    EssentialImage ,
-    DisabledOrInactive ,
-    NonEssentialImage,
-    Logo,
-    Background,
-    Ignore,
+    None("0") ,
+    RegularText("1") ,
+    LargeText("2") ,
+    BoldText("3") ,
+    EssentialImage("4") ,
+    DisabledOrInactive("5") ,
+    NonEssentialImage("6"),
+    Logo("7"),
+    Background("8"),
+    Ignore("9"),
+    ;
+
+    private String value;
+
+    AccessibilityRegionType(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getMeters() {
+        return value;
+    }
 }
