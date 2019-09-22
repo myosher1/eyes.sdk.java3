0ddd25c079b5a5032a016829d5a7e3f1b0fa7c5d
Michael Yosher <michael.yosher@applitools.com>
2019-09-22 10:57:03 +0300

    update enum

--- eyes.common.java/src/main/java/com/applitools/eyes/AccessibilityRegionType.java
+++ eyes.common.java/src/main/java/com/applitools/eyes/AccessibilityRegionType.java
@@ -3,26 +3,10 @@ package com.applitools.eyes;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum AccessibilityRegionType {
-    None("0") ,
-    RegularText("1") ,
-    LargeText("2") ,
-    BoldText("3") ,
-    EssentialImage("4") ,
-    DisabledOrInactive("5") ,
-    NonEssentialImage("6"),
-    Logo("7"),
-    Background("8"),
-    Ignore("9"),
-    ;
-
-    private String value;
-
-    AccessibilityRegionType(String value) {
-        this.value = value;
-    }
-
-    @JsonValue
-    public String getMeters() {
-        return value;
-    }
+    None,
+    IgnoreContrast,
+    RegularText,
+    LargeText,
+    BoldText,
+    GraphicalObject,
 }
