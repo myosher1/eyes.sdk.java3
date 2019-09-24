448ad70626a2ff2653290fadda59c9c54d0e4d5a
Itai Bar-Haim <itai.barhaim@applitools.com>
2019-09-24 12:23:23 +0300

    improved TestIE11, implemented InternetExplorerScreenshotImageProvider. WIP.

--- eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestBrowserStack.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestBrowserStack.java
@@ -1,11 +1,13 @@
 package com.applitools.eyes.selenium;
 
+import com.applitools.eyes.RectangleSize;
 import com.applitools.eyes.selenium.fluent.Target;
 import com.applitools.eyes.utils.TestUtils;
 import com.applitools.utils.GeneralUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -17,8 +19,13 @@ import static com.applitools.eyes.selenium.TestsDataProvider.BROWSERSTACK_USERNA
 
 public class TestBrowserStack {
 
-    @Test
-    public void TestIE11() {
+    @DataProvider
+    public static Object[][] stitchModeDP() {
+        return new Object[][]{{StitchMode.CSS}, {StitchMode.SCROLL}};
+    }
+
+    @Test(dataProvider = "stitchModeDP")
+    public void TestIE11(StitchMode stitchMode) {
 
         Eyes eyes = new Eyes();
         TestUtils.setupLogging(eyes);
@@ -36,7 +43,9 @@ public class TestBrowserStack {
         try {
             driver = new RemoteWebDriver(new URL(BROWSERSTACK_SELENIUM_URL), caps);
             driver.get("https://applitools.github.io/demo/TestPages/FramesTestPage/");
-            eyes.open(driver, "TestBrowserStack", "TesIE11");
+            eyes.setStitchMode(stitchMode);
+            eyes.setBatch(TestsDataProvider.batchInfo);
+            eyes.open(driver, "TestBrowserStack", "TesIE11_" + stitchMode, new RectangleSize(800, 600));
             eyes.check("viewport", Target.window().fully(false).sendDom(false));
             eyes.check("full page", Target.window().fully(true).sendDom(false));
             eyes.close();
@@ -46,6 +55,7 @@ public class TestBrowserStack {
             if (driver != null) {
                 driver.quit();
             }
+            eyes.abort();
         }
     }
 
