4991418e8a9c09d2753264fb1d50e28a5acebe52
Michael Yosher <michael.yosher@applitools.com>
2019-11-19 15:06:32 +0200

    Fixed IOS + trying to run all tests

--- eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/MobileNativeTests.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/MobileNativeTests.java
@@ -34,66 +34,66 @@ public class MobileNativeTests {
         eyes.setBatch(TestDataProvider.batchInfo);
         return eyes;
     }
-//
-//    @Test
-//    public void AndroidNativeAppTest1() throws Exception {
-//
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//
-//        capabilities.setCapability("platformName", "Android");
-//        capabilities.setCapability("deviceName", "Android Emulator");
-//        capabilities.setCapability("platformVersion", "6.0");
-//        capabilities.setCapability("app", "http://saucelabs.com/example_files/ContactManager.apk");
-//        capabilities.setCapability("clearSystemFiles", true);
-//        capabilities.setCapability("noReset", true);
-//
-//        Eyes eyes = initEyes(capabilities);
-//
-//        WebDriver driver = new AndroidDriver(new URL(SAUCE_SELENIUM_URL), capabilities);
-//
-//        try {
-//            eyes.open(driver, "Mobile Native Tests", "Android Native App 1");
-//            eyes.checkWindow("Contact list");
-//            eyes.close();
-//        } finally {
-//            driver.quit();
-//            eyes.abortIfNotClosed();
-//        }
-//    }
-//
-//    @Test
-//    public void AndroidNativeAppTest2() throws MalformedURLException, InterruptedException {
-//
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//
-//        capabilities.setCapability("platformName", "Android");
-//        capabilities.setCapability("platformVersion", "7.1");
-//        capabilities.setCapability("deviceName", "Samsung Galaxy S8 WQHD GoogleAPI Emulator");
-//        capabilities.setCapability("automationName", "uiautomator2");
-//
-//        capabilities.setCapability("app", "https://applitools.bintray.com/Examples/app-debug.apk");
-//
-//        capabilities.setCapability("appPackage", "com.applitoolstest");
-//        capabilities.setCapability("appActivity", "com.applitoolstest.ScrollActivity");
-//        capabilities.setCapability("newCommandTimeout", 600);
-//
-//        Eyes eyes = initEyes(capabilities);
-//
-//        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL(SAUCE_SELENIUM_URL), capabilities);
-//
-//        try {
-//            eyes.open(driver, "Mobile Native Tests", "Android Native App 2");
-//            Thread.sleep(10000);
-//
-//            MobileElement scrollableElement = driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().scrollable(true)"));
-//
-//            eyes.check("Main window with ignore", Target.region(scrollableElement).ignore(scrollableElement));
-//            eyes.close(false);
-//        } finally {
-//            driver.quit();
-//            eyes.abortIfNotClosed();
-//        }
-//    }
+
+    @Test
+    public void AndroidNativeAppTest1() throws Exception {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Android Emulator");
+        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("app", "http://saucelabs.com/example_files/ContactManager.apk");
+        capabilities.setCapability("clearSystemFiles", true);
+        capabilities.setCapability("noReset", true);
+
+        Eyes eyes = initEyes(capabilities);
+
+        WebDriver driver = new AndroidDriver(new URL(SAUCE_SELENIUM_URL), capabilities);
+
+        try {
+            eyes.open(driver, "Mobile Native Tests", "Android Native App 1");
+            eyes.checkWindow("Contact list");
+            eyes.close();
+        } finally {
+            driver.quit();
+            eyes.abortIfNotClosed();
+        }
+    }
+
+    @Test
+    public void AndroidNativeAppTest2() throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "7.1");
+        capabilities.setCapability("deviceName", "Samsung Galaxy S8 WQHD GoogleAPI Emulator");
+        capabilities.setCapability("automationName", "uiautomator2");
+
+        capabilities.setCapability("app", "https://applitools.bintray.com/Examples/app-debug.apk");
+
+        capabilities.setCapability("appPackage", "com.applitoolstest");
+        capabilities.setCapability("appActivity", "com.applitoolstest.ScrollActivity");
+        capabilities.setCapability("newCommandTimeout", 600);
+
+        Eyes eyes = initEyes(capabilities);
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL(SAUCE_SELENIUM_URL), capabilities);
+
+        try {
+            eyes.open(driver, "Mobile Native Tests", "Android Native App 2");
+            Thread.sleep(10000);
+
+            MobileElement scrollableElement = driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().scrollable(true)"));
+
+            eyes.check("Main window with ignore", Target.region(scrollableElement).ignore(scrollableElement));
+            eyes.close(false);
+        } finally {
+            driver.quit();
+            eyes.abortIfNotClosed();
+        }
+    }
 
     @Test
     public void iOSNativeAppTest() throws Exception {
@@ -106,13 +106,9 @@ public class MobileNativeTests {
         caps.setCapability("platformName", "iOS");
         caps.setCapability("browserName", "");
         caps.setCapability("app", "https://applitools.bintray.com/Examples/HelloWorldiOS_1_0.zip");
-//        caps.setCapability("clearSystemFiles", true);
-//        caps.setCapability("noReset", true);
 
         Eyes eyes = initEyes(caps);
         eyes.setLogHandler(new StdoutLogHandler());
-//        setCapabilities(eyes, caps, "iOSNativeAppTest");
-//        TestUtils.setupLogging(eyes);
         eyes.setBatch(TestDataProvider.batchInfo);
 
 
