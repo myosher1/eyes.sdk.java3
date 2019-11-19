9e2df15fb16a579127887bdba197a1aee1755576
Michael Yosher <michael.yosher@applitools.com>
2019-11-19 14:52:17 +0200

    tyring to run IOS only

--- eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/MobileNativeTests.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/MobileNativeTests.java
@@ -1,5 +1,6 @@
 package com.applitools.eyes.selenium;
 
+import com.applitools.eyes.StdoutLogHandler;
 import com.applitools.eyes.selenium.fluent.Target;
 import com.applitools.eyes.utils.TestUtils;
 import io.appium.java_client.MobileBy;
@@ -20,7 +21,7 @@ import static com.applitools.eyes.selenium.TestDataProvider.*;
 public class MobileNativeTests {
 
     private void setCapabilities(Eyes eyes, DesiredCapabilities capabilities, String methodName) {
-        capabilities.setCapability("username",  SAUCE_USERNAME);
+        capabilities.setCapability("username", SAUCE_USERNAME);
         capabilities.setCapability("accesskey", SAUCE_ACCESS_KEY);
         capabilities.setCapability("name", methodName);
     }
@@ -33,66 +34,66 @@ public class MobileNativeTests {
         eyes.setBatch(TestDataProvider.batchInfo);
         return eyes;
     }
-
-    @Test
-    public void AndroidNativeAppTest1() throws Exception {
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "Android Emulator");
-        capabilities.setCapability("platformVersion", "6.0");
-        capabilities.setCapability("app", "http://saucelabs.com/example_files/ContactManager.apk");
-        capabilities.setCapability("clearSystemFiles", true);
-        capabilities.setCapability("noReset", true);
-
-        Eyes eyes = initEyes(capabilities);
-
-        WebDriver driver = new AndroidDriver(new URL(SAUCE_SELENIUM_URL), capabilities);
-
-        try {
-            eyes.open(driver, "Mobile Native Tests", "Android Native App 1");
-            eyes.checkWindow("Contact list");
-            eyes.close();
-        } finally {
-            driver.quit();
-            eyes.abortIfNotClosed();
-        }
-    }
-
-    @Test
-    public void AndroidNativeAppTest2() throws MalformedURLException, InterruptedException {
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "7.1");
-        capabilities.setCapability("deviceName", "Samsung Galaxy S8 WQHD GoogleAPI Emulator");
-        capabilities.setCapability("automationName", "uiautomator2");
-
-        capabilities.setCapability("app", "https://applitools.bintray.com/Examples/app-debug.apk");
-
-        capabilities.setCapability("appPackage", "com.applitoolstest");
-        capabilities.setCapability("appActivity", "com.applitoolstest.ScrollActivity");
-        capabilities.setCapability("newCommandTimeout", 600);
-
-        Eyes eyes = initEyes(capabilities);
-
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL(SAUCE_SELENIUM_URL), capabilities);
-
-        try {
-            eyes.open(driver, "Mobile Native Tests", "Android Native App 2");
-            Thread.sleep(10000);
-
-            MobileElement scrollableElement = driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().scrollable(true)"));
-
-            eyes.check("Main window with ignore", Target.region(scrollableElement).ignore(scrollableElement));
-            eyes.close(false);
-        } finally {
-            driver.quit();
-            eyes.abortIfNotClosed();
-        }
-    }
+//
+//    @Test
+//    public void AndroidNativeAppTest1() throws Exception {
+//
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//
+//        capabilities.setCapability("platformName", "Android");
+//        capabilities.setCapability("deviceName", "Android Emulator");
+//        capabilities.setCapability("platformVersion", "6.0");
+//        capabilities.setCapability("app", "http://saucelabs.com/example_files/ContactManager.apk");
+//        capabilities.setCapability("clearSystemFiles", true);
+//        capabilities.setCapability("noReset", true);
+//
+//        Eyes eyes = initEyes(capabilities);
+//
+//        WebDriver driver = new AndroidDriver(new URL(SAUCE_SELENIUM_URL), capabilities);
+//
+//        try {
+//            eyes.open(driver, "Mobile Native Tests", "Android Native App 1");
+//            eyes.checkWindow("Contact list");
+//            eyes.close();
+//        } finally {
+//            driver.quit();
+//            eyes.abortIfNotClosed();
+//        }
+//    }
+//
+//    @Test
+//    public void AndroidNativeAppTest2() throws MalformedURLException, InterruptedException {
+//
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//
+//        capabilities.setCapability("platformName", "Android");
+//        capabilities.setCapability("platformVersion", "7.1");
+//        capabilities.setCapability("deviceName", "Samsung Galaxy S8 WQHD GoogleAPI Emulator");
+//        capabilities.setCapability("automationName", "uiautomator2");
+//
+//        capabilities.setCapability("app", "https://applitools.bintray.com/Examples/app-debug.apk");
+//
+//        capabilities.setCapability("appPackage", "com.applitoolstest");
+//        capabilities.setCapability("appActivity", "com.applitoolstest.ScrollActivity");
+//        capabilities.setCapability("newCommandTimeout", 600);
+//
+//        Eyes eyes = initEyes(capabilities);
+//
+//        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL(SAUCE_SELENIUM_URL), capabilities);
+//
+//        try {
+//            eyes.open(driver, "Mobile Native Tests", "Android Native App 2");
+//            Thread.sleep(10000);
+//
+//            MobileElement scrollableElement = driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().scrollable(true)"));
+//
+//            eyes.check("Main window with ignore", Target.region(scrollableElement).ignore(scrollableElement));
+//            eyes.close(false);
+//        } finally {
+//            driver.quit();
+//            eyes.abortIfNotClosed();
+//        }
+//    }
 
     @Test
     public void iOSNativeAppTest() throws Exception {
@@ -108,19 +109,23 @@ public class MobileNativeTests {
 //        caps.setCapability("clearSystemFiles", true);
 //        caps.setCapability("noReset", true);
 
-        Eyes eyes = new Eyes();
+        Eyes eyes = initEyes(caps);
+        eyes.setLogHandler(new StdoutLogHandler());
         setCapabilities(eyes, caps, "iOSNativeAppTest");
         TestUtils.setupLogging(eyes);
         eyes.setBatch(TestDataProvider.batchInfo);
 
-        WebDriver driver = new IOSDriver(new URL(SAUCE_SELENIUM_URL), caps);
 
+        WebDriver driver = null;
         try {
+            driver = new IOSDriver(new URL(SAUCE_SELENIUM_URL), caps);
             eyes.open(driver, "Mobile Native Tests", "iOS Native App");
             eyes.checkWindow("checkWindow");
             eyes.close();
         } finally {
-            driver.quit();
+            if (driver != null) {
+                driver.quit();
+            }
             eyes.abortIfNotClosed();
         }
     }
